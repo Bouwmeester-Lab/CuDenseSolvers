391a0fad6132fe7386242eb7ded1e75dadc692e6
emorell96 <emorell96@hotmail.com>
2025-08-06 14:34:49 +0200

    latest portfile

--- ports/CuDenseSolvers/portfile.cmake
+++ ports/CuDenseSolvers/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
 # Install headers from CuDenseSolvers/include/
 file(INSTALL
     "${SOURCE_PATH}/CuDenseSolvers/include"
-    DESTINATION "${CURRENT_PACKAGES_DIR}"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/CuDenseSolvers"
     FILES_MATCHING
         PATTERN "*.h"
         PATTERN "*.hpp"
