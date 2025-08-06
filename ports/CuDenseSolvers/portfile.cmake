76e22b32e299037e4931539fac792cb4b4578f1d
emorell96 <emorell96@hotmail.com>
2025-08-06 12:51:23 +0200

    adding portfile to install library with vcpkg

--- ports/CuDenseSolvers/portfile.cmake
+++ ports/CuDenseSolvers/portfile.cmake
@@ -2,12 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Bouwmeester-Lab/CuDenseSolvers
     REF master  # Or a tag like v0.1.0
-    SHA512 0  # Placeholder, use actual hash below
+    SHA512 f32e25750c4cdfa880319558604ef7e1ced89cc505acad75a5f1624796cdf6873e835ac125bc53dd0af5622f7a7cfd388dff29e566c8efb12d739fbc93d865a8
 )
 
-# Install headers from CuDenseSolvers/ folder
+# Install headers from CuDenseSolvers/include/
 file(INSTALL
-    "${SOURCE_PATH}/CuDenseSolvers"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/include"
+    "${SOURCE_PATH}/CuDenseSolvers/include"
+    DESTINATION "${CURRENT_PACKAGES_DIR}"
     FILES_MATCHING PATTERN "*.hpp"
 )
